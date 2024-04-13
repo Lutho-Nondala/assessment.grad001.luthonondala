3d82fb1cd410a75f6ea27d2ec2fece605ae6c877
Lutho <lnondala07@gmail.com>
2024-04-13 17:22:12 +0200

    Database Commit

--- src/main/java/com/enviro/assessment/grad001/luthonondala/repository/TextFilesRepository.java
+++ src/main/java/com/enviro/assessment/grad001/luthonondala/repository/TextFilesRepository.java
@@ -1,4 +1,9 @@
 package com.enviro.assessment.grad001.luthonondala.repository;
 
-public interface TextFilesRepository {
+import com.enviro.assessment.grad001.luthonondala.entity.TextFiles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TextFilesRepository extends JpaRepository<TextFiles, Long> {
 }
