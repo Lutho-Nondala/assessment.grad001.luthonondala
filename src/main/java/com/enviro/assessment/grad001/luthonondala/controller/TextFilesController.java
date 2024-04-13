ed91319413ca00a412f2ce4bb2601779a7318376
Lutho <lnondala07@gmail.com>
2024-04-13 20:18:06 +0200

    Service and Controller Commit (Create + Read Methods are complete so far on Controllers)

--- src/main/java/com/enviro/assessment/grad001/luthonondala/controller/TextFilesController.java
+++ src/main/java/com/enviro/assessment/grad001/luthonondala/controller/TextFilesController.java
@@ -1,4 +1,40 @@
 package com.enviro.assessment.grad001.luthonondala.controller;
 
+import com.enviro.assessment.grad001.luthonondala.service.TextFilesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@CrossOrigin
+@RestController
+@RequestMapping("enviro/")
 public class TextFilesController {
+    @Autowired
+    private TextFilesService SERVICE;
+
+    @PostMapping(value = {"create"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<String> create(@RequestPart("text")MultipartFile file){
+        try{
+            String a = this.SERVICE.create(file);
+            return ResponseEntity.status(HttpStatus.OK).body(a);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @GetMapping("read/{id}")
+    public ResponseEntity<?> read(@PathVariable long id){
+        byte[] a =  this.SERVICE.read(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("text/xml;charset=UTF-8"))
+                .body(a);
+    }
+
+
+
+
 }
--- src/main/java/com/enviro/assessment/grad001/luthonondala/service/TextFilesService.java
+++ src/main/java/com/enviro/assessment/grad001/luthonondala/service/TextFilesService.java
@@ -1,4 +1,61 @@
 package com.enviro.assessment.grad001.luthonondala.service;
 
+import com.enviro.assessment.grad001.luthonondala.entity.TextFiles;
+import com.enviro.assessment.grad001.luthonondala.repository.TextFilesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
 public class TextFilesService {
+    @Autowired
+    private TextFilesRepository REPO;
+
+    public String create(MultipartFile file) throws IOException {
+        TextFiles a = this.REPO.save(new TextFiles.Builder()
+                        .name(file.getOriginalFilename())
+                        .type(file.getContentType())
+                        .fileByte(file.getBytes())
+                        .build());
+
+        if (a != null){
+            return a.getName() + " uploaded successfully.";
+        }
+
+        return null;
+    }
+
+    public byte[] read(long id){
+        byte[] a = this.REPO.findById(id).get().getFileByte();
+
+        if (a != null){
+            return a;
+        }
+
+        return null;
+    }
+
+    public TextFiles getById(long id){
+        return this.REPO.findById(id).get();
+    }
+
+    public String update(MultipartFile file, long id) throws IOException {
+        TextFiles a = this.getById(id);
+
+        TextFiles b = this.REPO.save(new TextFiles.Builder()
+                        .copy(a)
+                        .name(file.getOriginalFilename())
+                        .type(file.getContentType())
+                        .fileByte(file.getBytes())
+                        .build());
+
+        if (b != null){
+            return b.getName() + " uploaded successfully.";
+        }
+
+        return null;
+    }
+
 }
