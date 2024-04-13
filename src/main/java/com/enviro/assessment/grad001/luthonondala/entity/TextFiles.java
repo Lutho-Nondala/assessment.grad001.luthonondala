23e0199c5de8513ec59889e1679162cdc5e9dda1
Lutho <lnondala07@gmail.com>
2024-04-13 16:10:40 +0200

    Initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/luthonondala/entity/TextFiles.java
@@ -0,0 +1,4 @@
+package com.enviro.assessment.grad001.luthonondala.entity;
+
+public class TextFiles {
+}
---
+++ src/main/java/com/enviro/assessment/grad001/luthonondala/repository/TextFilesRepository.java
@@ -0,0 +1,4 @@
+package com.enviro.assessment.grad001.luthonondala.repository;
+
+public interface TextFilesRepository {
+}
